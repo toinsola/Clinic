bd7a19892f48eae34fb53828db171f59d38bdc8b
Your Name <you@example.com>
2022-09-28 09:53:25 -0500

    2.) Created Database

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def create_server_connection(host_name, user_name, user_password):
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            password=user_password,
+            password=user_password
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -15,7 +15,17 @@ def create_server_connection(host_name, user_name, user_password):
 
     return connection
 
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
 
 
 
-create_server_connection("localhost", "root", "student")
+#Callout section
+connection = create_server_connection("localhost", "root", "student")
+database_query = "CREATE DATABASE Central_Clinic"
+create_database(connection, database_query)
